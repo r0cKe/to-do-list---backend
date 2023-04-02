fb0972ee73c0ed313d546ce0ee82294631e24d7f
Ritik Sabhrwal <ritik@schoolhype.in>
2023-04-02 16:28:19 +0530

    first commit

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-require("dotenv").config();
+// require("dotenv").config();
 const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
@@ -11,7 +11,6 @@ connectToDB();
 
 const app = express();
 
-// app.set("view engine", "ejs");
 app.use(cors());
 app.use(express.json());
 app.use(express.static("public"));
